7e55e450fd7eb0a8ee23e5b4e4ce69c1f1be1ff8
Barathkumarke <barathke.visualallies@gmail.com>
2025-05-16 19:01:23 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("user_registration", "root", "Pass@123!", {
-  host: "ec2-54-198-64-144.compute-1.amazonaws.com",
+  host: "localhost",
   dialect: "mysql",
 });
 
